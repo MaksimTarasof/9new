30d4a003a6fd7b97f72f7095b07ba375c26fac5e
Max <eparis@atlassian.com>
2023-02-07 23:52:18 +0400

    project2 init

---
+++ src/main/java/Radio.java
@@ -0,0 +1,73 @@
+public class Radio {
+    private int currentVolume;
+
+    //увеличение громкости
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+    // уменьшение громкости
+
+    public void reduceVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    //переключение станций вперед
+    public int currentNumberStation;
+
+    public int getCurrentNumberStation() {
+        return currentNumberStation;
+    }
+
+    public void setCurrentNumberStation(int newCurrentNumberStation) {
+        if (newCurrentNumberStation < 0) {
+            return;
+        }
+        if (newCurrentNumberStation > 9) {
+            return;
+        }
+        currentNumberStation = newCurrentNumberStation;
+    }
+
+    public void increaseNumberStation() {
+        if (currentNumberStation < 9) {
+            currentNumberStation = currentNumberStation + 1;
+        }
+        //ниже под вопросом
+        if (currentNumberStation > 9) {
+            currentNumberStation = 0;
+        }
+    }
+
+    //переключение станций назад
+
+    public void reduceNumberStation() {
+        if (currentNumberStation > 0) {
+            currentNumberStation = currentNumberStation - 1;
+        }
+        //ниже под вопросом
+        if (currentNumberStation < 0) {
+            currentNumberStation = 9;
+        }
+    }
+}
+
+
+
